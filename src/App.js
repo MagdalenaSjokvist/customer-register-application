da30d36ad18ad1c55e599ca74008ba4d5238c628
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-22 13:50:51 +0200

    Import Switch and first Route in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 
 function App() {
-  return <div>
-    <h1>En början</h1>
-  </div>
+	return (
+		<div>
+			<Switch>
+				<Route path="/">
+					<h1>En början</h1>
+				</Route>
+			</Switch>
+		</div>
+	)
 }
 
 export default App
