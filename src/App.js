c5a34f95b6c9ca1c83cd93a276fc6af2e9556e59
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-22 14:35:45 +0200

    Handle register user in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
+import UserKit from "./data/UserKit"
 
 function App() {
+	const userKit = new UserKit()
+
+	function handleRegisterUser() {
+		userKit.register(
+			"Magdalena",
+			"Sjökvist",
+			"magdalena.sjokvist+123@gmail.com",
+			"test123456",
+			"test AB",
+			"0"
+		)
+	}
+
 	return (
 		<div>
 			<Switch>
 				<Route path="/">
-					<h1>En början</h1>
-					<button onClick={handleCreateUser}>Registrera dig</button>
+					<h1>Startsida</h1>
+					<h3>Registrera ny användare</h3>
+					<button onClick={handleRegisterUser}>Registrera dig</button>
 				</Route>
 			</Switch>
 		</div>
