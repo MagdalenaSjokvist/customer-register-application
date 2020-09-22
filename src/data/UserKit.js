f5c73f1cefb7d35afe29b8a690a789714b2208fd
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-22 14:20:45 +0200

    Add register method to UserKit

--- src/data/UserKit.js
+++ src/data/UserKit.js
@@ -4,4 +4,43 @@ const AUTH_URL = `${ROOT_URL}auth/`
 const LOGIN_URL = `${ROOT_URL}api-token-auth/`
 // `${ROOT_URL}api/v1/customers`
 // `${ROOT_URL}auth/users/activate/`
-// `${ROOT_URL}auth/users/activate/`
+
+export default class {
+	async register(
+		firstName,
+		lastName,
+		email,
+		password,
+		organisationName,
+		organisationKind
+	) {
+		const url = `${AUTH_URL}users/`
+		const payload = {
+			firstName,
+			lastName,
+			email,
+			password,
+			organisationName,
+			organisationKind,
+		}
+		return fetch(url, {
+			method: "POST",
+			headers: this.getPublicHeaders(),
+			body: JSON.stringify(payload),
+		})
+	}
+
+	//Genom att skriva ut header (Content-Type) med en funktion minskar risken att vi skriver fel (om vi skriver fler det gånger)
+	getPublicHeaders() {
+		return {
+			"Content-Type": "application/json",
+		}
+	}
+
+	getPrivateHeaders() {
+		return {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${this.getToken()}`,
+		}
+	}
+}
