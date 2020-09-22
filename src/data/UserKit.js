7f2203cbc9b6acc5aed225bc4a6d7d1b2d64dfff
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-22 14:10:27 +0200

    Add URL:s to UserKit.js

---
+++ src/data/UserKit.js
@@ -0,0 +1,7 @@
+const ROOT_URL = "https://frebi.willandskill.eu/"
+const API_URL = `${ROOT_URL}api/v1/`
+const AUTH_URL = `${ROOT_URL}auth/`
+const LOGIN_URL = `${ROOT_URL}api-token-auth/`
+// `${ROOT_URL}api/v1/customers`
+// `${ROOT_URL}auth/users/activate/`
+// `${ROOT_URL}auth/users/activate/`
