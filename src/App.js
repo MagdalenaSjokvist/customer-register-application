dfa2b45b47e952ce49a1ad0e6cec53cd114c0099
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 19:50:54 +0200

    Add styling for AppContainer (wrapping element)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, { useState } from "react"
+import React from "react"
 import { Route, Switch } from "react-router-dom"
+import { GlobalStyles } from "./styles/GlobalStyles"
+import styled from "styled-components"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import UserHomePage from "./pages/UserHomePage"
-import { GlobalStyles } from "./styles/GlobalStyles"
+
+const AppContainer = styled.div`
+	padding: 2rem;
+	background: rgba(109, 170, 205);
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+`
 
 function App() {
 	return (
-		<div>
+		<AppContainer>
 			<GlobalStyles />
 			<Switch>
 				<Route path="/home">
 					<UserHomePage />
 				</Route>
+
 				<Route path="/login">
 					<LoginPage />
 				</Route>
+
 				<Route path="/">
 					<RegisterPage />
 				</Route>
 			</Switch>
-		</div>
+		</AppContainer>
 	)
 }
 
