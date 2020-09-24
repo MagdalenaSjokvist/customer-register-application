5b7394c938edbf21f6556a7655f908b6b9240d0f
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 20:18:01 +0200

    Add h2, label, input styling to GlobalStyles.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,16 +5,35 @@ export const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   font-family: 'Poppins', sans-serif;
-
 }
 
 h1 {
-  font-size: 34px;
+  font-size: 1.7rem;
   margin: 2vh 0 3vh 0;
+}
+
+h2 {
+  font-size: 1.5rem;
+  margin: 2vh 0 3vh 0;
+}
+
+h3 {
+  
 }
 
 button {
   padding: 0.5rem;
-  margin-top: 1.5rem; 
+  margin-top: 1rem; 
+}
+
+label{
+  font-size: 0.9rem;
+}
+
+input {
+  height: 30px;
+  width: 45vw;
+  min-width: 200px;
+  margin: 0.2rem 0;
 }
 `
