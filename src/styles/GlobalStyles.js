67a83c7c62a2e359c1995d51dcc542b74903d104
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 19:50:22 +0200

    remove som styling

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,16 +5,11 @@ export const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   font-family: 'Poppins', sans-serif;
-  padding: 0.4rem;
-}
 
-.appContainer {
 }
 
 button {
   padding: 0.5rem;
-  margin-left: 0.4rem;
-  margin-top: 0.5rem; 
-  
+  margin-top: 1.5rem; 
 }
 `
