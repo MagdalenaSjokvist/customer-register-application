a3737f2de2dd56381a192a849f825a6b077b5ffa
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 23:46:22 +0200

    Add styling for table

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -25,7 +25,8 @@ h3 {
 button {
   padding: 0.5rem;
   max-width: 150px;
-  margin-top: 0.5rem; 
+  margin-top: 0.8rem; 
+  margin-bottom: 0.8rem;
   font-size: 0.8rem;
   border: none;
   border-radius: 3px;
@@ -35,11 +36,12 @@ button {
 
 label{
   font-size: 0.8rem;
+  width: 100%;
 }
 
 input {
   height: 32px;
-  width: 45vw;
+  width: 100%;
   min-width: 200px;
   margin: 0.2rem 0 0.5rem 0 ;
   border: none;
@@ -48,9 +50,40 @@ input {
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1);
 }
 
-th {
-  font-size: 0.6rem;
-  padding-right: 2rem;
+th{
+  /* padding-right: 3rem;
+  text-align: left; */
 }
 
+thead{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3B628C;
+  color: white;
+  height: 40px;
+  width: 55vw;
+  min-width: 400px;
+  border-radius: 4px;
+  font-size: small;
+  padding: 1rem;
+}
+
+tr {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 30px;
+  width: 55vw;
+  min-width: 400px;
+  padding: 2rem 0rem 2rem 1rem;
+  font-size: small;
+}
+
+
+td {
+  font-size: smaller;
+}
 `
