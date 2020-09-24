182f389e5220cbb248f3cb5aaaba90850cb95748
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 18:48:27 +0200

    Create LoginPage.jsx for activating and login user

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react"
+import { useHistory } from "react-router-dom"
+import UserKit from "../data/UserKit"
+
+export default function LoginPage() {
+	const history = useHistory()
+	const urlParameters = new URLSearchParams(history.location.search) // Use URL Search Params to parse the query parameters from the url
+	const [uid, setUid] = useState(urlParameters.get("uid"))
+	const [token, setToken] = useState(urlParameters.get("token"))
+	// const uid = params.get("uid")
+	// const token = params.get("token")
+
+	const [loginEmail, setLoginEmail] = useState("")
+	const [loginPassword, setLoginPassword] = useState("")
+
+	//Skapa ny instans av klassen UserKit
+	const userKit = new UserKit()
+
+	//AKTIVERA KONTO
+	function handleActivateAccount() {
+		userKit.activateAccount(uid, token).then(() => {
+			setUid(null)
+			setToken(null)
+			history.push("/login") //Så snart activateUser är klar - skicka användaren till /login
+		})
+	}
+
+	//LOGGA IN
+	function handleLogin() {
+		userKit
+			.login(loginEmail, loginPassword)
+			.then((res) => res.json())
+			.then((data) => {
+				userKit.setToken(data.token)
+				// console.log(loginEmail, loginPassword)
+				// console.log(data)
+				history.push("/home")
+			})
+	}
+
+	return (
+		<div>
+			{/* Om uid och token finns - rendera ut första diven, annars rendera ut den andra diven */}
+			{uid && token ? (
+				<div>
+					<h1>Aktivera ditt konto</h1>
+					<p>
+						Klicka på knappen nedan för att aktivera ditt konto och logga in.
+					</p>
+					<button onClick={handleActivateAccount}>Aktivera ditt konto</button>
+				</div>
+			) : (
+				<div>
+					<h1>Logga in</h1>
+					<input
+						type="email"
+						placeholder="Ex. info@mail.se"
+						value={loginEmail}
+						onChange={(e) => setLoginEmail(e.target.value)}
+					/>
+					<input
+						type="password"
+						placeholder="Lösenord"
+						value={loginPassword}
+						onChange={(e) => setLoginPassword(e.target.value)}
+					/>
+					<button onClick={handleLogin}>Logga in</button>
+				</div>
+			)}
+		</div>
+	)
+}
