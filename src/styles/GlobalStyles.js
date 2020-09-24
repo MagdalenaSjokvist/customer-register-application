b98c2ecd343730e12e5209ab2a6054f1d51d0b58
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 18:49:44 +0200

    Create GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  padding: 0.3rem;
+}
+
+.appContainer {
+}
+
+button {
+  padding: 0.5rem;
+  margin-right: 1em;
+}
+`
