78588338ade0c57e5fe29e5a68879c9f16d35dda
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-24 18:35:30 +0200

    Create RegisterPage.jsx for all register code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from "react"
 import { Route, Switch, useHistory } from "react-router-dom"
 import UserKit from "./data/UserKit"
 import styled from "styled-components"
+import RegisterPage from "./pages/RegisterPage"
 
 const InputLabel = styled.label`
 	display: flex;
 	flex-direction: column;
 `
-
 // const appContainer = styled.main`
 // 	box-sizing: border-box;
 // 	margin: 0 auto;
@@ -15,13 +15,6 @@ const InputLabel = styled.label`
 // `
 
 function App() {
-	const [firstName, setFirstName] = useState("")
-	const [lastName, setLastName] = useState("")
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [organisationName, setOrganisationName] = useState("")
-	const [organisationKind, setOrganisationKind] = useState("")
-	const [confirmationMessage, setConfirmationMessage] = useState("")
 	const history = useHistory()
 	const urlParameters = new URLSearchParams(history.location.search) // Use URL Search Params to parse the query parameters from the url
 	const [uid, setUid] = useState(urlParameters.get("uid"))
@@ -33,69 +26,9 @@ function App() {
 	const [loginPassword, setLoginPassword] = useState("")
 
 	const [customerList, setCustomerList] = useState("")
+
 	//Skapa ny instans av klassen UserKit
 	const userKit = new UserKit()
-
-	const renderInputFields = (inputFieldsArray) => {
-		// console.log(inputFieldsArray)
-		return inputFieldsArray.map((inputField, index) => {
-			return (
-				<InputLabel key={index}>
-					{inputField[0]}
-					<input
-						type={inputField[1]}
-						placeholder={inputField[2]}
-						value={inputField[3]}
-						onChange={(event) => {
-							inputField[4](event.target.value)
-						}}
-					/>
-				</InputLabel>
-			)
-		})
-	}
-
-	const inputFieldsArray = [
-		["Förnamn", "text", "", firstName, setFirstName],
-		["Efternamn", "text", "", lastName, setLastName],
-		["E-post", "email", "Ex. info@mail.se", email, setEmail],
-		["Lösenord", "password", "Minst 8 tecken", password, setPassword],
-		["Organisationsnamn", "text", "", organisationName, setOrganisationName],
-		[
-			"Organisationstyp",
-			"text",
-			"Välj 0, 1 eller 2",
-			organisationKind,
-			setOrganisationKind,
-		],
-	]
-
-	//REGISTRERA KONTO
-	function handleRegisterAccount() {
-		userKit
-			.registerAccount(
-				firstName,
-				lastName,
-				email,
-				password,
-				organisationName,
-				organisationKind
-			)
-			.then(
-				setConfirmationMessage(
-					"Tack för din registrering! Du får strax ett mejl till din registrerade e-postadress. Klicka på länken i mejlet för att aktivera ditt konto."
-				)
-			)
-			.then(
-				setFirstName(""),
-				setLastName(""),
-				setEmail(""),
-				setPassword(""),
-				setOrganisationName(""),
-				setOrganisationKind("")
-			)
-		// .then(history.push("/login"))
-	}
 
 	//AKTIVERA KONTO
 	function handleActivateAccount() {
@@ -158,6 +91,7 @@ function App() {
 						</div>
 						<div>
 							<h4>Lägg till en ny kund</h4>
+
 							<button onClick={handleCreateCustomer}>Lägg till</button>
 						</div>
 					</div>
@@ -197,10 +131,7 @@ function App() {
 					</div>
 				</Route>
 				<Route path="/">
-					<h1>Registrera dig</h1>
-					<p>{confirmationMessage}</p>
-					{renderInputFields(inputFieldsArray)}
-					<button onClick={handleRegisterAccount}>Registrera dig</button>
+					<RegisterPage />
 				</Route>
 			</Switch>
 		</div>
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react"
+import UserKit from "../data/UserKit"
+import styled from "styled-components"
+
+const InputLabel = styled.label`
+	display: flex;
+	flex-direction: column;
+`
+export default function RegisterPage() {
+	const [firstName, setFirstName] = useState("")
+	const [lastName, setLastName] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [organisationName, setOrganisationName] = useState("")
+	const [organisationKind, setOrganisationKind] = useState("")
+	const [confirmationMessage, setConfirmationMessage] = useState("")
+	const userKit = new UserKit()
+
+	const renderInputFields = (inputFieldsArray) => {
+		// console.log(inputFieldsArray)
+		return inputFieldsArray.map((inputField, index) => {
+			return (
+				<InputLabel key={index}>
+					{inputField[0]}
+					<input
+						type={inputField[1]}
+						placeholder={inputField[2]}
+						value={inputField[3]}
+						onChange={(event) => {
+							inputField[4](event.target.value)
+						}}
+					/>
+				</InputLabel>
+			)
+		})
+	}
+
+	const inputFieldsArray = [
+		["Förnamn", "text", "", firstName, setFirstName],
+		["Efternamn", "text", "", lastName, setLastName],
+		["E-post", "email", "Ex. info@mail.se", email, setEmail],
+		["Lösenord", "password", "Minst 8 tecken", password, setPassword],
+		["Organisationsnamn", "text", "", organisationName, setOrganisationName],
+		[
+			"Organisationstyp",
+			"text",
+			"Välj 0, 1 eller 2",
+			organisationKind,
+			setOrganisationKind,
+		],
+	]
+
+	//REGISTRERA KONTO
+	function handleRegisterAccount() {
+		userKit
+			.registerAccount(
+				firstName,
+				lastName,
+				email,
+				password,
+				organisationName,
+				organisationKind
+			)
+			.then(
+				setConfirmationMessage(
+					"Tack för din registrering! Du får strax ett mejl till din registrerade e-postadress. Klicka på länken i mejlet för att aktivera ditt konto."
+				)
+			)
+			.then(
+				setFirstName(""),
+				setLastName(""),
+				setEmail(""),
+				setPassword(""),
+				setOrganisationName(""),
+				setOrganisationKind("")
+			)
+		// .then(history.push("/login"))
+	}
+
+	return (
+		<div>
+			<h1>Registrera dig</h1>
+			<p>{confirmationMessage}</p>
+			{renderInputFields(inputFieldsArray)}
+			<button onClick={handleRegisterAccount}>Registrera dig</button>
+		</div>
+	)
+}
