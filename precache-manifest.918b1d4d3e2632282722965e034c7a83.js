b5cd3215ed537e6a5bfb3246a0d0f76af54680be
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-29 14:47:20 +0200

    Updates

---
+++ precache-manifest.918b1d4d3e2632282722965e034c7a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d0b15f363276b86f2da3a23d9506d1",
+    "url": "/js3-business-project/index.html"
+  },
+  {
+    "revision": "b13fd88a5d283f39de10",
+    "url": "/js3-business-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "702900dc1687b10f1a38",
+    "url": "/js3-business-project/static/js/2.ae5b7d88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js3-business-project/static/js/2.ae5b7d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13fd88a5d283f39de10",
+    "url": "/js3-business-project/static/js/main.9367ba47.chunk.js"
+  },
+  {
+    "revision": "3d9ca2dcc4c76d507afb",
+    "url": "/js3-business-project/static/js/runtime-main.6ac3dbef.js"
+  }
+]);
